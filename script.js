877a054ca64a9af02194e122919cdae5ba8aefed
Lalit-som <Lalitsom27@gmail.com>
2017-09-05 16:57:16 +0530

    added images in img

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function loadTextImg(){
     var img = new Image();
     img.src = document.getElementById('text-url-input').value
     var ctx = textCanvas.getContext('2d');
+    ctx.clearRect(0, 0, textCanvas.width, textCanvas.height);
     img.onload = function() {
       ctx.drawImage(img, 0, 0);
       img.style.display = 'none';
@@ -26,6 +27,7 @@ function loadProfileImg(){
     var img = new Image();
     img.src = document.getElementById('profile-url-input').value
     var ctx = profileCanvas.getContext('2d');
+    ctx.clearRect(0, 0, profileCanvas.width, profileCanvas.height);
     img.onload = function() {
       ctx.drawImage(img, 0, 0);
       img.style.display = 'none';
@@ -35,12 +37,35 @@ function loadProfileImg(){
 
 var tctx;
 var pctx;
-var data;
+var textData;
+var profileData;
 function mapProfileOnText(){
     tctx = textCanvas.getContext('2d');
     pctx = profileCanvas.getContext('2d');
 
-    var imageData = tctx.getImageData(0, 0, textCanvas.width, textCanvas.height);
-    data = imageData.data;
+    var textImageData = tctx.getImageData(0, 0, textCanvas.width, textCanvas.height);
+    textData = textImageData.data; //rgba data of image
+    
+    var profileImageData = pctx.getImageData(0, 0, profileCanvas.width, profileCanvas.height);
+    profileData = profileImageData.data; //rgba data of image
+    
+    
+    var index = 100*100;
+    
+    while(index<textData.length){
+      if(textData[index]<250 || textData[index+1]<250 || textData[index+2]<250){
+        //if(profileData[index]<250 && profileData[index+1]<250 && profileData[index+2]<250)
+        {
+          textData[index] = profileData[index];
+          textData[index+1] = profileData[index+1];
+          textData[index+2] = profileData[index+2];  
+        }
+        
+      }
+      index += 4; // skip the alpha pixel
+    }
+    
+    tctx.putImageData(textImageData,0,0);
+    
 
 }
